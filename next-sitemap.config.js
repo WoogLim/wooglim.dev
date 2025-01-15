26daf6e663fceb37743ca1a89a1617f533f93c52
WoogLim <elegy74@gmail.com>
2025-01-15 18:21:44 +0900

    modify sitemap and robot

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -8,13 +8,12 @@ module.exports = {
       { userAgent: "*", allow: "/" },
     ],
   },
-  exclude: ["/note"], // /note를 sitemap에서 제외
-  generateHost: false, // Host 디렉티브 생성을 비활성화
+  generateHost: false,
   additionalPaths: async (config) => [
     {
-      loc: "/snippets", // 경로 추가
-      changefreq: "daily", // 페이지 변경 빈도 (필요에 따라 조정)
-      priority: 1.0, // SEO 우선 순위 (0.1~1.0)
+      loc: "/snippets",
+      changefreq: "daily",
+      priority: 1.0,
     },
   ],
 };
