9a84e95ad965e545f7d7f6bf820005633312ad19
WoogLim <elegy74@gmail.com>
2025-01-15 18:20:10 +0900

    modify sitemap and robot

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,9 +5,12 @@ module.exports = {
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [
-      { userAgent: "*", disallow: "/note" },
-      { userAgent: "*", allow: "/" },
+      {
+        userAgent: "*",
+        disallow: ["/note"], // /note만 비허용
+      },
     ],
   },
-  exclude: ["/note"],
+  exclude: ["/note"], // /note를 sitemap에서 제외
+  generateHost: false, // Host 디렉티브 생성을 비활성화
 };
